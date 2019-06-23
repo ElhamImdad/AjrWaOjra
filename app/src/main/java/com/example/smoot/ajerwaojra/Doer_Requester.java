c2f0d66d0af563844e701afd8a80d3edc0dcb4ea
ElhamImdad <Elham.imdad@gmail.com>
2019-06-23 14:58:08 +0300

    sign up Button

---
+++ app/src/main/java/com/example/smoot/ajerwaojra/Doer_Requester.java
@@ -0,0 +1,14 @@
+package com.example.smoot.ajerwaojra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Doer_Requester extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doer__requester);
+    }
+}
---
+++ app/src/main/java/com/example/smoot/ajerwaojra/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.smoot.ajerwaojra;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        configureSignUpButton();
+    }
+
+    private void configureSignUpButton(){
+        Button btnSignUp = (Button)findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Doer_Requester.class));
+            }
+        });
+    }
+}
