b942eccc46a1125ba19a5cfaeebd4805ce7a5a92
raghad-basha <roro.bahsa@gmail.com>
2019-06-27 16:06:55 +0300

    lll

--- app/src/main/java/com/example/smoot/ajerwaojra/Fragments/FatwaFragment.java
+++ app/src/main/java/com/example/smoot/ajerwaojra/Fragments/FatwaFragment.java
@@ -1,4 +1,5 @@
 package com.example.smoot.ajerwaojra.Fragments;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
