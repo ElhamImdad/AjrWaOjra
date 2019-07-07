85c63bbd2d0946856b7c34d2193c0268dd906593
ElhamImdad <Elham.imdad@gmail.com>
2019-07-07 15:18:08 +0300

    modify in UI

--- app/src/main/java/com/example/smoot/ajerwaojra/Activities/SplashActivity.java
+++ app/src/main/java/com/example/smoot/ajerwaojra/Activities/SplashActivity.java
@@ -1,20 +1,20 @@
 package com.example.smoot.ajerwaojra.Activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
-import com.example.smoot.ajerwaojra.Activities.MainActivity;
 import com.example.smoot.ajerwaojra.R;
 
 public class SplashActivity extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        progressBar = findViewById(R.id.progressBar);
         Thread thread = new Thread(){
             @Override
             public void run() {
@@ -22,6 +22,8 @@ public class SplashActivity extends AppCompatActivity {
                     sleep(4000);
                     Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
+                    progressBar.getProgressDrawable().setColorFilter(
+                            Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
                     finish();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
